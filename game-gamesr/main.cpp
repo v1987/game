50ff2120258bb44efe96cff9f25e613f60a9a338
eclipser <eclipser@163.com>
2014-12-29 11:25:20 +0800

    游戏服务器

---
+++ game-gamesr/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Vicky.H
+ *
+ * Created on 2014年12月29日, 上午11:03
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
