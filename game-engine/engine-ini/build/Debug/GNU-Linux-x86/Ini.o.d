cfd97ec8b299ffbbeb590eeedcbca00e73177727
eclipser <eclipser@163.com>
2014-12-30 15:47:22 +0800

    .ini配置文件读取

---
+++ game-engine/engine-ini/build/Debug/GNU-Linux-x86/Ini.o.d
@@ -0,0 +1,10 @@
+build/Debug/GNU-Linux-x86/Ini.o: Ini.cpp Ini.h ../engine-common/Type.h \
+ ../engine-common/BaseType.h ../engine-common/Assertx.h
+
+Ini.h:
+
+../engine-common/Type.h:
+
+../engine-common/BaseType.h:
+
+../engine-common/Assertx.h:
