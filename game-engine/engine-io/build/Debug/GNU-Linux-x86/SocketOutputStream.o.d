f2eca85d667500dc3ce27133d09c4dd1cfd8b9fa
eclipser <eclipser@163.com>
2014-12-30 18:25:19 +0800

    socket io

---
+++ game-engine/engine-io/build/Debug/GNU-Linux-x86/SocketOutputStream.o.d
@@ -0,0 +1,17 @@
+build/Debug/GNU-Linux-x86/SocketOutputStream.o: SocketOutputStream.cpp \
+ SocketOutputStream.h ../engine-socket/Socket.h \
+ ../engine-socket/SocketAPI.h ../engine-socket/../engine-common/Type.h \
+ ../engine-socket/../engine-common/BaseType.h \
+ ../engine-socket/../engine-common/Assertx.h
+
+SocketOutputStream.h:
+
+../engine-socket/Socket.h:
+
+../engine-socket/SocketAPI.h:
+
+../engine-socket/../engine-common/Type.h:
+
+../engine-socket/../engine-common/BaseType.h:
+
+../engine-socket/../engine-common/Assertx.h:
--- game-engine/engine-io/nbproject/Makefile-Release.mk
+++ game-engine/engine-io/nbproject/Makefile-Release.mk
@@ -34,7 +34,8 @@ include Makefile
 OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
-OBJECTFILES=
+OBJECTFILES= \
+	${OBJECTDIR}/SocketOutputStream.o
 
 
 # C Compiler Flags
@@ -59,7 +60,12 @@ LDLIBSOPTIONS=
 
 ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libengine-io.${CND_DLIB_EXT}: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
-	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libengine-io.${CND_DLIB_EXT} ${OBJECTFILES} ${LDLIBSOPTIONS} -shared -fPIC
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/libengine-io.${CND_DLIB_EXT} ${OBJECTFILES} ${LDLIBSOPTIONS} -shared -fPIC
+
+${OBJECTDIR}/SocketOutputStream.o: SocketOutputStream.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -fPIC  -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/SocketOutputStream.o SocketOutputStream.cpp
 
 # Subprojects
 .build-subprojects:
